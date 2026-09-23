1189ce783a833b438d0378867c6dcbb72f14c3d6
Profound Dono <ryandonokarijo@outlook.com>
2026-09-23 17:20:04 +0700

    Update 02_customer_metrics.sql

--- sql/02_customer_metrics.sql
+++ sql/02_customer_metrics.sql
@@ -1,1 +1,81 @@
+-- ============================================================
+-- Olist E-Commerce Analysis
+-- 02 - Customer Metrics
+-- ============================================================
+-- Purpose:
+-- Measure the size, purchasing frequency, and revenue
+-- contribution of the customer base.
+-- ============================================================
 
+
+-- ============================================================
+-- 1. Unique Customers
+-- ============================================================
+-- How many unique customers are represented in the dataset?
+
+SELECT COUNT(DISTINCT customer_unique_id) FROM customers
+
+-- ============================================================
+-- 2. One-Time Customers
+-- ============================================================
+-- How many customers placed exactly one order?
+
+SELECT
+COUNT(*) AS one_time_customers
+FROM (
+    SELECT
+        c.customer_unique_id
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    GROUP BY c.customer_unique_id
+    HAVING COUNT(o.order_id) = 1
+);
+
+-- ============================================================
+-- 3. Repeat Customers
+-- ============================================================
+-- How many customers placed more than one order?
+
+SELECT
+    COUNT(*) AS one_time_customers
+FROM (
+    SELECT
+        c.customer_unique_id
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    GROUP BY c.customer_unique_id
+    HAVING COUNT(o.order_id) > 1
+);
+
+-- ============================================================
+-- 4. Repeat Customer Rate
+-- ============================================================
+-- What percentage of unique customers are repeat customers?
+
+...
+
+-- ============================================================
+-- 5. Average Orders per Customer
+-- ============================================================
+-- How many orders does the average customer place?
+
+SELECT
+round(avg(ye), 2) AS one_time_customers
+FROM (
+    SELECT
+    count(o.order_id) ye, 
+    count(c.customer_unique_id)
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    GROUP BY c.customer_unique_id
+);
+
+-- ============================================================
+-- 6. Average Revenue per Customer
+-- ============================================================
+-- What is the average revenue generated per unique customer?
+
+...
