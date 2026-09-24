3c3cf06b5c0f866d43619c0e2b952b28b913b956
Profound Dono <ryandonokarijo@outlook.com>
2026-09-24 14:40:58 +0700

    Update 08_geography_analysis.sql

--- sql/08_geography_analysis.sql
+++ sql/08_geography_analysis.sql
@@ -11,36 +11,57 @@
 -- ============================================================
 -- 1. Customers by State
 -- ============================================================
--- Which Brazilian states have the most unique customers?
 
-
+SELECT
+c.customer_state,
+COUNT(c.customer_unique_id) AS customer_count
+FROM customers c
+FULL OUTER JOIN orders o
+ON o.customer_id = c.customer_id
+GROUP BY c.customer_state
+ORDER BY COUNT(c.customer_unique_id) DESC LIMIT 3
 
 -- ============================================================
 -- 2. Revenue by State
 -- ============================================================
--- Which states generate the most revenue?
 
-
+SELECT
+    c.customer_state,
+    SUM(op.payment_value) AS revenue
+FROM order_payments op 
+JOIN orders o
+    ON op.order_id = o.order_id
+JOIN customers c
+    ON o.customer_id = c.customer_id
+GROUP BY c.customer_state
+ORDER BY revenue DESC LIMIT 3
 
 -- ============================================================
 -- 3. Repeat Customer Rate by State
 -- ============================================================
--- How does repeat purchasing vary by state?
 
-
+SELECT
+    c.customer_state as states,
+    count(o.order_id) as rc
+FROM order_items oi
+JOIN orders o
+    ON oi.order_id = o.order_id
+JOIN customers c
+    ON o.customer_id = c.customer_id
+GROUP BY c.customer_state
+having count(o.order_id) > 1
 
 -- ============================================================
 -- 4. Revenue per Customer by State
 -- ============================================================
--- How much revenue does the average unique customer
--- generate in each state?
 
-
-
--- ============================================================
--- 5. Geographic Customer Behavior
--- ============================================================
--- Are there meaningful differences in customer purchasing
--- behavior between states?
-
-
+SELECT
+    c.customer_state,
+    round(avg(op.payment_value), 2) AS avg_revenue
+FROM order_payments op 
+JOIN orders o
+    ON op.order_id = o.order_id
+JOIN customers c
+    ON o.customer_id = c.customer_id
+GROUP BY c.customer_state
+ORDER BY  avg_revenue desc
