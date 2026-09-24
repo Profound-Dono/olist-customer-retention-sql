c51c103521883a09668ffe37a69ba286eb9b1f79
Profound Dono <ryandonokarijo@outlook.com>
2026-09-24 16:06:37 +0700

    Update 07_category_analysis.sql

--- sql/07_category_analysis.sql
+++ sql/07_category_analysis.sql
@@ -13,30 +13,36 @@
 -- ============================================================
 -- Which product categories have the most unique customers?
 
+SELECT
+p.product_category_name AS categories,
+count(c.customer_unique_id) AS customers
+FROM products p
+JOIN order_items oi 
+ON p.product_id = oi.product_id
+JOIN orders o
+ON o.order_id = oi.order_id 
+JOIN customers c
+ON o.customer_id = c.customer_id 
+GROUP BY p.product_category_name
+ORDER BY COUNT(c.customer_unique_id) desc
 
 
 -- ============================================================
--- 2. Revenue by Product Category
--- ============================================================
--- Which categories generate the most item revenue?
-
-
--- ============================================================
--- 3. Repeat Customer Rate by Category
+-- 2. Repeat Customer Rate by Category
 -- ============================================================
 -- Which categories have the highest repeat customer rates?
 
 
 
 -- ============================================================
--- 4. Lowest Repeat Customer Rates by Category
+-- 3. Lowest Repeat Customer Rates by Category
 -- ============================================================
 -- Which categories have the lowest repeat customer rates?
 
 
 
 -- ============================================================
--- 5. Repeat-Customer Revenue by Category
+-- 4. Repeat-Customer Revenue by Category
 -- ============================================================
 -- Which product categories generate the most revenue from
 -- customers classified as repeat customers?
