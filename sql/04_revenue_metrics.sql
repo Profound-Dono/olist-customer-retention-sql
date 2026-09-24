564ca96d92a79f53acdfc70ab5ba8a970f6676d3
Profound Dono <ryandonokarijo@outlook.com>
2026-09-24 13:34:03 +0700

    Update 04_revenue_metrics.sql

--- sql/04_revenue_metrics.sql
+++ sql/04_revenue_metrics.sql
@@ -1,1 +1,66 @@
+-- ============================================================
+-- Olist E-Commerce Analysis
+-- 04 - Revenue Metrics
+-- ============================================================
+-- Purpose:
+-- Analyze total revenue, revenue trends, product categories,
+-- and geographic revenue distribution.
+-- ============================================================
+
+
+-- ============================================================
+-- 1. Total Revenue
+-- ============================================================
+-- What is the total customer payment value represented
+-- in the dataset?
+
+SELECT ROUND(SUM(payment_value), 2) FROM order_payments
+
+-- ============================================================
+-- 2. Revenue by Month
+-- ============================================================
+-- How does revenue change over time?
+
+SELECT
+    DATE_TRUNC('month', o.order_purchase_timestamp)::date AS month,
+    ROUND(SUM(op.payment_value), 2) AS revenue
+FROM orders o
+JOIN order_payments op
+    ON o.order_id = op.order_id
+GROUP BY 1
+ORDER BY 1
+;
+
+-- ============================================================
+-- 3. Revenue by Product Category
+-- ============================================================
+-- Which product categories generate the most item revenue?
+
+SELECT
+p.product_category_name,
+SUM(oi.price) AS revenue
+FROM order_items oi
+JOIN products p
+ON p.product_id = oi.product_id
+GROUP BY p.product_category_name
+ORDER BY sum(oi.price) desc                      
+
+-- ============================================================
+-- 4. Revenue by Customer State
+-- ============================================================
+-- Which Brazilian states generate the most revenue?
+
+SELECT customer_state, sum(payment_value) AS revenue FROM orders
+INNER JOIN order_payments
+ON orders.order_id = order_payments.order_id 
+INNER JOIN customers
+ON customers.customer_id = orders.customer_id
+GROUP BY customer_state
+
+-- ============================================================
+-- 5. Average Order Value
+-- ============================================================
+-- What is the average value of a completed order?
+
+SELECT ROUND(AVG(payment_value), 2) FROM order_payments
 
