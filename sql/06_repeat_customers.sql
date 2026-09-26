f36e2c557ca7270a17f3cfa52878c5241a3fd77d
Profound Dono <ryandonokarijo@outlook.com>
2026-09-26 11:37:56 +0700

    Update 06_repeat_customers.sql

--- sql/06_repeat_customers.sql
+++ sql/06_repeat_customers.sql
@@ -11,9 +11,20 @@
 -- ============================================================
 -- 1. Overall Repeat Customer Rate
 -- ============================================================
--- What percentage of customers are repeat customers?
 
-
+SELECT
+ROUND(100.0*COUNT(*)/(SELECT COUNT(DISTINCT customer_unique_id)
+FROM customers),2) AS repeat_customer_rate
+FROM
+(
+SELECT
+c.customer_unique_id
+FROM orders o
+JOIN customers c
+ON o.customer_id = c.customer_id
+GROUP BY c.customer_unique_id
+HAVING COUNT(o.order_id) > 1
+) AS repeat_customers;
 
 -- ============================================================
 -- 2. Repeat Customer Rate by Month
